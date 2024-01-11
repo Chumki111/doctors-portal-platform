50ae6305330ce4191ce939d91ada08158babcad4
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-12 01:21:27 +0600

    add card 3 of services section

--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -1,5 +1,6 @@
 import fluoride from '../../assets/icons/fluoride 1.png'
 import cavity from '../../assets/icons/cavity 1.png'
+import whitening from '../../assets/icons/whitening 1.png'
 const Services = () => {
     return (
         <div className="my-16">
@@ -22,6 +23,15 @@ const Services = () => {
                    <div className='py-6 text-center'>
                    <div className='flex justify-center py-3 pb-4 rounded-md '><img src={cavity} alt=""  className=''/></div>
                     <h2 className='text-[#3A4256] font-semibold'>Cavity Filling</h2>
+                    <p>Lorem Ipsum is simply dummy printing and <br /> typesetting indust Ipsum has been the</p>
+                   </div>
+                </div>
+
+                {/* card-3 */}
+                <div className='shadow-2xl before:block before:-left-1 before:-top-1 before:bg-[#19D3AE] before:absolute before:h-0 before:w-0 before:hover:w-[100%] before:hover:h-[100%]  before:duration-500 before:-z-40 after:block after:-right-1 after:-bottom-1 after:bg-[#19D3AE] after:absolute after:h-0 after:w-0 after:hover:w-[100%] after:hover:h-[100%] after:duration-500 after:-z-40 bg-white relative inline-block rounded-md'>
+                   <div className='py-6 text-center'>
+                   <div className='flex justify-center py-3 pb-4 rounded-md '><img src={whitening} alt=""  className=''/></div>
+                    <h2 className='text-[#3A4256] font-semibold'>Teeth Whitening</h2>
                     <p>Lorem Ipsum is simply dummy printing and <br /> typesetting indust Ipsum has been the</p>
                    </div>
                 </div>
