11fc96f41cf26f70f5e24903d0d1aaa43f6e144d
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-11 20:40:20 +0600

    page create

---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/Pages/Reviews/Reviews.jsx
@@ -0,0 +1,11 @@
+
+
+const Reviews = () => {
+    return (
+        <div>
+            <h2>Reviews</h2>
+        </div>
+    );
+};
+
+export default Reviews;
