3d385281e86514b936cba75133b755321c989d4b
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-12 22:29:52 +0600

    add testimonial slider card

--- src/Pages/Home/Testimonial.jsx
+++ src/Pages/Home/Testimonial.jsx
@@ -1,4 +1,5 @@
 import image1 from '../../assets/doll2.png'
+import { GiClover } from "react-icons/gi";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { BiCommand } from "react-icons/bi";
@@ -35,6 +36,7 @@ const Testimonial = () => {
       >
         <SwiperSlide>
         <div className='shadow-2xl bg-white rounded-xl'>
+        <GiClover className='text-[#19D3AE] animate-ping text-2xl'/>
                    <div className='py-6 text-center px-7'>
                   <p className='text-left'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
                    </div>
@@ -48,14 +50,86 @@ const Testimonial = () => {
                    </div>
                    </div>
                 </div></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <div className='shadow-2xl bg-white rounded-xl'>
+          <GiClover className='text-[#19D3AE] animate-ping text-2xl'/>
+                   <div className='py-6 text-center px-7'>
+                  <p className='text-left'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
+                   </div>
+                   <div className='flex gap-3 px-7'>
+                   <div className='w-20 h-20 '>
+                   <img src={image1} alt="" className='rounded-full border-2 p-1 border-[#19D3AE]'/>
+                   </div>
+                   <div className='text-start'>
+                    <h2 className='font-medium text-[#19D3AE]'>Chumki Khatun</h2>
+                    <h5>Rajshahi,Bangladesh</h5>
+                   </div>
+                   </div>
+                </div>
+                </SwiperSlide>
+        <SwiperSlide>
+        <div className='shadow-2xl bg-white rounded-xl'>
+        <GiClover className='text-[#19D3AE] animate-ping text-2xl'/>
+                   <div className='py-6 text-center px-7'>
+                  <p className='text-left'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
+                   </div>
+                   <div className='flex gap-3 px-7'>
+                   <div className='w-20 h-20 '>
+                   <img src={image1} alt="" className='rounded-full border-2 p-1 border-[#19D3AE]'/>
+                   </div>
+                   <div className='text-start'>
+                    <h2 className='font-medium text-[#19D3AE]'>Chumki Khatun</h2>
+                    <h5>Rajshahi,Bangladesh</h5>
+                   </div>
+                   </div>
+                </div>
+        </SwiperSlide>
+        <SwiperSlide><div className='shadow-2xl bg-white rounded-xl'>
+        <GiClover className='text-[#19D3AE] animate-ping text-2xl'/>
+                   <div className='py-6 text-center px-7'>
+                  <p className='text-left'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
+                   </div>
+                   <div className='flex gap-3 px-7'>
+                   <div className='w-20 h-20 '>
+                   <img src={image1} alt="" className='rounded-full border-2 p-1 border-[#19D3AE]'/>
+                   </div>
+                   <div className='text-start'>
+                    <h2 className='font-medium text-[#19D3AE]'>Chumki Khatun</h2>
+                    <h5>Rajshahi,Bangladesh</h5>
+                   </div>
+                   </div>
+                </div></SwiperSlide>
+        <SwiperSlide><div className='shadow-2xl bg-white rounded-xl'>
+        <GiClover className='text-[#19D3AE] animate-ping text-2xl'/>
+                   <div className='py-6 text-center px-7'>
+                  <p className='text-left'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
+                   </div>
+                   <div className='flex gap-3 px-7'>
+                   <div className='w-20 h-20 '>
+                   <img src={image1} alt="" className='rounded-full border-2 p-1 border-[#19D3AE]'/>
+                   </div>
+                   <div className='text-start'>
+                    <h2 className='font-medium text-[#19D3AE]'>Chumki Khatun</h2>
+                    <h5>Rajshahi,Bangladesh</h5>
+                   </div>
+                   </div>
+                </div></SwiperSlide>
+        <SwiperSlide><div className='shadow-2xl bg-white rounded-xl'>
+        <GiClover className='text-[#19D3AE] animate-ping text-2xl'/>
+                   <div className='py-6 text-center px-7'>
+                  <p className='text-left'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
+                   </div>
+                   <div className='flex gap-3 px-7'>
+                   <div className='w-20 h-20 '>
+                   <img src={image1} alt="" className='rounded-full border-2 p-1 border-[#19D3AE]'/>
+                   </div>
+                   <div className='text-start'>
+                    <h2 className='font-medium text-[#19D3AE]'>Chumki Khatun</h2>
+                    <h5>Rajshahi,Bangladesh</h5>
+                   </div>
+                   </div>
+                </div></SwiperSlide>
+       
       </Swiper>
           
         </div>
