aa81075a59b6f021baca3f8f18da69729b8df1bf
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-12 17:25:42 +0600

    added a icon with testimonial

--- src/Pages/Home/Testimonial.jsx
+++ src/Pages/Home/Testimonial.jsx
@@ -1,11 +1,14 @@
-
+import { BiCommand } from "react-icons/bi";
 
 const Testimonial = () => {
     return (
         <div className="my-14">
-           <div className="pl-10">
-           <h2 className="text-xl font-semibold text-[#19D3AE]">Testimonial</h2>
+           <div className="md:pl-10 flex justify-between items-center">
+          <div>
+          <h2 className="text-xl font-semibold text-[#19D3AE]">Testimonial</h2>
             <h1 className="text-3xl font-medium">What Our Patients Says</h1>
+          </div>
+            <BiCommand className="text-[#19D3AE] cursor-copy" size={60}/>
            </div>
         </div>
     );
