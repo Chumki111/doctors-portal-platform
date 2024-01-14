4959c7b7b1d4955104a8c8fab8f002b9102857e8
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-14 20:09:59 +0600

    add a date

--- src/Pages/Appointment/Appointment.jsx
+++ src/Pages/Appointment/Appointment.jsx
@@ -10,9 +10,15 @@ const Appointment = () => {
             key: 'selection'
         }
     ]);
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+           
+        }
+    ]);
     return (
         <div>
-            <section className="flex min-h-[700px] w-full items-center justify-center px-8 " style={{ backgroundImage: 'url("https://i.ibb.co/MPhXGcj/landing-bg.png")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+            <div className="flex min-h-[700px] w-full items-center justify-center px-8 " style={{ backgroundImage: 'url("https://i.ibb.co/MPhXGcj/landing-bg.png")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
                 <div className="flex w-full max-w-6xl gap-10 lg:flex-row flex-col items-center justify-between">
                     <div className="max-w-md md:space-y-6 sm:space-y-5 space-y-4">
 
@@ -29,7 +35,13 @@ const Appointment = () => {
                         <img src={chair} />
                     </div>
                 </div>
-            </section>
+            </div>
+            <div>
+                <div className='text-center'>
+                    <h2 className='text-color font-medium text-xl'>Available Services on {date[0].startDate.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}</h2>
+                    <p className='text-lg'>Please select a service.</p>
+                </div>
+            </div>
         </div>
     );
 };
