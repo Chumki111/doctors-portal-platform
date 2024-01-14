e128289042c62f91cc79c28b5a17b55bbff8424d
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-14 19:47:40 +0600

    add date range and a calendar of appoinment page

--- src/Pages/Appointment/Appointment.jsx
+++ src/Pages/Appointment/Appointment.jsx
@@ -1,22 +1,35 @@
-
+import { DateRange } from 'react-date-range';
 import chair from '../../assets/chair 1.png'
+import { useState } from 'react';
 
 const Appointment = () => {
+    const [state, setState] = useState([
+        {
+            startDate: new Date(),
+            endDate: null,
+            key: 'selection'
+        }
+    ]);
     return (
         <div>
-            <section className="flex min-h-[700px] w-full items-center justify-center px-8 " style={{ backgroundImage: 'url("https://i.ibb.co/MPhXGcj/landing-bg.png")',backgroundRepeat: 'no-repeat',backgroundSize: 'cover'}}>
-        <div className="flex w-full max-w-6xl gap-10 lg:flex-row flex-col items-center justify-between">
-          <div className="max-w-md md:space-y-6 sm:space-y-5 space-y-4">
-            
-            
-            
-            
-          </div>
-          <div className="relative">  
-            <img src={chair}  />
-          </div>
-        </div>
-      </section> 
+            <section className="flex min-h-[700px] w-full items-center justify-center px-8 " style={{ backgroundImage: 'url("https://i.ibb.co/MPhXGcj/landing-bg.png")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                <div className="flex w-full max-w-6xl gap-10 lg:flex-row flex-col items-center justify-between">
+                    <div className="max-w-md md:space-y-6 sm:space-y-5 space-y-4">
+
+                        <DateRange
+                            editableDateInputs={true}
+                            onChange={item => setState([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={state}
+                        />
+
+
+                    </div>
+                    <div className="relative">
+                        <img src={chair} />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './Router/Router'
+// date-range 
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
