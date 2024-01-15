b16c0cac9c25e05a76fb9db4529866e7cbdde3f0
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-15 16:22:54 +0600

    check email and password feild

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,16 +2,24 @@ import { Link } from "react-router-dom";
 
 
 const Login = () => {
+    const handleLogin =(e) =>{
+  e.preventDefault();
+  const form = e.target;
+  const email = form.email.value;
+  const password = form.password.value;
+  console.log(email,password);
+    }
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center">
             <div className="w-full max-w-md p-8 space-y-3 rounded-xl border shadow-md font-sans">
             <h1 className="text-2xl font-bold text-center">Login</h1>
-            <form action="" className="space-y-6">
+            <form onSubmit={handleLogin} action="" className="space-y-6">
                 <div className="space-y-1 text-sm">
                     <label htmlFor="username" className="block dark:text-gray-400">
-                        Username
+                        Email
                     </label>
-                    <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md border  dark:border-[#19D3AE]  dark:text-[#19D3AE] focus:dark:border-[#19D3AE]"/>
+                    <input type="email" name="email" id="email" placeholder="User_Email" className="w-full px-4 py-3 rounded-md border  dark:border-[#19D3AE]  dark:text-[#19D3AE] focus:dark:border-[#19D3AE]"/>
+                    
                 </div>
                 <div className="space-y-1 text-sm">
                     <label htmlFor="password" className="block dark:text-gray-400">
@@ -22,7 +30,7 @@ const Login = () => {
                         <a rel="noopener noreferrer" href="#">Forgot Password?</a>
                     </div>
                 </div>
-                <button className="block w-full p-3 text-center bg-black text-white rounded-sm ">Sign in</button>
+                <button type="submit" className="block w-full p-3 text-center bg-black text-white rounded-sm ">Sign in</button>
                 <p className="text-black text-center sm:px-6">
                 New To Doctors_Portal ?
                 <Link to="/register" className="hover:underline text-color pl-2">
