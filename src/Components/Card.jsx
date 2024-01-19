9f16e4428477ce393dfd1872ca1cc0255d4e03d4
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-19 10:54:33 +0600

    add card number four

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const Card = ({title,description,icon:Icon}) => {
-    console.log(typeof(description));
+    
     return (
         <div className='shadow-2xl before:block before:-left-1 before:-top-1 before:bg-[#448C74] before:absolute before:h-0 before:w-0 before:hover:w-[100%] before:hover:h-[100%]  before:duration-500 before:-z-40 after:block after:-right-1 after:-bottom-1 after:bg-[#448C74] after:absolute after:h-0 after:w-0 after:hover:w-[100%] after:hover:h-[100%] after:duration-500 after:-z-40 bg-white relative inline-block rounded-md hover:shadow-2xl'>
                     <div className='py-6 text-center'>
