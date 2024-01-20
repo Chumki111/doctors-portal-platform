973974d7fe8d1a4cb381807ed498251baf1969cc
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-20 23:01:14 +0600

    add title on appointment page with react helmet

--- src/Pages/Appointment/Appointment.jsx
+++ src/Pages/Appointment/Appointment.jsx
@@ -1,6 +1,7 @@
 import { DateRange } from 'react-date-range';
 import chair from '../../assets/chair 1.png'
 import { useState } from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Appointment = () => {
     const [state, setState] = useState([
@@ -21,6 +22,9 @@ const Appointment = () => {
     
     return (
         <div>
+            <Helmet>
+            <title>Doctor_Portal | Appointment</title>
+        </Helmet>
             <div className="flex min-h-[700px] w-full items-center justify-center px-8 " style={{ backgroundImage: 'url("https://i.ibb.co/MPhXGcj/landing-bg.png")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
                 <div className="flex w-full max-w-6xl gap-10 lg:flex-row flex-col items-center justify-between">
                     <div className="max-w-md md:space-y-6 sm:space-y-5 space-y-4">
