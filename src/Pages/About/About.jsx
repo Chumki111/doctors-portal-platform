af08443b307809d4b074123f418663b64447fa10
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-27 19:18:57 +0600

    remove all card from about page and added some new

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,46 +1,32 @@
 // import fluoride from '../../assets/icons/fluoride 1.png'
-import { GrYoga } from "react-icons/gr";
-import { IoFitnessSharp } from "react-icons/io5";
-import { MdSportsGymnastics } from "react-icons/md";
-import { GiWeightLiftingUp } from "react-icons/gi";
-import { BsEmojiSmile } from "react-icons/bs";
-import { MdDirectionsBike } from "react-icons/md";
-import Card from "../../Components/Card";
+
 import { Helmet } from "react-helmet-async";
+import { IoMdArrowDropright } from "react-icons/io";
+import { IoFitnessOutline } from "react-icons/io5";
+import { GrYoga } from "react-icons/gr";
+import { GiBoxingGloveSurprise } from "react-icons/gi";
+import { Link } from "react-router-dom";
 const About = () => {
     return (
         <>
         <Helmet>
             <title>Doctor_Portal | About</title>
         </Helmet>
-        <div className="my-16">
-
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-14'>
-                {/* card-1 */}
-
-                <Card title="Yoga" icon={GrYoga} description="Yoga is a holistic discipline that originated in ancient India, encompassing physical, mental, and spiritual practices." />
-                {/* card-2 */}
-                <Card title="Cross Fit" icon={IoFitnessSharp} description='"Fit" generally refers to a state of good physical health and well-being achieved through regular exercise, proper nutrition, and an active lifestyle' />
-
-                {/* card-3 */}
-                <Card title="gym nastic" icon={MdSportsGymnastics} description='Gymnastics is a sport that combines strength, flexibility, balance, and coordination through a series of dynamic and graceful movements.' />
-
-
-
-
-                {/* Weightlifting Card */}
-                <Card title="Weightlifting" icon={GiWeightLiftingUp} description='Weightlifting is a strength training activity that builds muscle mass and increases strength. Grab those weights and sculpt your body!' />
-
-
-
-                {/* Mental Health Card */}
-                <Card title="Mental Health" icon={BsEmojiSmile} description='Prioritize your mental well-being. Learn about stress management, mindfulness, and techniques to promote a positive and resilient mindset' />
-
-
-                {/* Cycling Card */}
-                <Card title="Cycling" icon={MdDirectionsBike} description='Cycling is a low-impact exercise that improves cardiovascular health and leg strength. Get on your bike and enjoy the ride!' />
-
-            </div>
+        <div className="">
+        <div className="flex justify-between px-6">
+        <div className="flex items-center my-5 cursor-pointer">
+        <Link to={'/'} className="text-xl font-semibold ">Home </Link>
+         <IoMdArrowDropright className="w-[22px]"/>
+         <h1 className="text-xl font-semibold text-[#448c74]">About Us</h1>
+        </div>
+        <div className="md:flex gap-5 items-center cursor-pointer hidden ">
+        <IoFitnessOutline className="text-4xl"/>
+        <GrYoga className="text-4xl"/>
+ <GiBoxingGloveSurprise className="text-4xl"/>
+        </div>
+        
+         </div>
+         <hr />
         </div>
         </>
     );
