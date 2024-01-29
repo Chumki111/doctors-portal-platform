8fbb4296ed8da609536bca4ee1e9cff0e79c480f
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-29 23:53:01 +0600

    add title reviews page

--- src/Pages/Reviews/Reviews.jsx
+++ src/Pages/Reviews/Reviews.jsx
@@ -1,7 +1,11 @@
+import { Helmet } from "react-helmet-async";
 import { IoMdArrowDropright } from "react-icons/io";
 const Reviews = () => {
     return (
         <div>
+             <Helmet>
+            <title>Doctor_Portal | Reviews</title>
+        </Helmet>
             <h2>Reviews</h2>
             <IoMdArrowDropright size={10} />
             <h3>
